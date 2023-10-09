b630b1ea347e9808011a704f6ff637a98d816fc2
Martin Andrei <andrei.martin28@outlook.com>
2023-10-09 15:15:56 +0300

    Django and HTMX #5 finished

--- films/views.py
+++ films/views.py
@@ -6,14 +6,15 @@ from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
 from django.contrib.auth import get_user_model
-from films.models import Film
 from django.views.generic.list import ListView
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.decorators.http import require_http_methods
+from django.contrib import messages
 
 from films.forms import RegisterForm
+from films.models import Film
 
 # Create your views here.
 class IndexView(TemplateView):
@@ -55,13 +56,15 @@ def check_username(request):
 def add_film(request):
     name = request.POST.get('filmname')
 
-    film = Film.objects.create(name=name)
+    #add film
+    film = Film.objects.get_or_create(name=name)[0]
 
     # add the film to the user's list
     request.user.films.add(film)
 
     # return template with all of the user's films
     films = request.user.films.all()
+    messages.success(request, f"Added {name} to list of films")
     return render(request, 'partials/film-list.html', {'films': films})
 
 @login_required
@@ -74,10 +77,17 @@ def delete_film(request, pk):
     films = request.user.films.all()
     return render(request, 'partials/film-list.html', {'films' : films})
  
- def search_film(request):
+def search_film(request):
     search_text = request.POST.get('search')
 
     # taxi drive match Taxi Driver
-    results = Film.objects.filter(name__icontains=search_text)
+    userfilms = request.user.films.all()
+    results = Film.objects.filter(name__icontains=search_text).exclude(
+        name__in = userfilms.values_list('name', flat=True)
+    )
     context = {'results': results}
-    return render(request, 'partials/search-results.html')
+    return render(request, 'partials/search-results.html', context)
+
+def clear(request):
+    return HttpResponse("")
+
